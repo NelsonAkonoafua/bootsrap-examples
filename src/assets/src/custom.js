b49abb2908815342d97a444a9eb6a1f1d10451a6
NelsonAkonoafua <nelsonosat@gmail.com>
2022-07-25 12:40:33 +0530

    enter new commit

---
+++ src/assets/src/custom.js
@@ -0,0 +1,7 @@
+
+const mobileBtn = document.getElementById('mobile-menu')
+    nav= document.querySelector('nav');
+    mobileBtnExit = document.getElement('mobile-exit');
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('menu-btn')  
+    })    
