e4b1ad7e631ee830aef7a42b8b65cdc49703130e
NelsonAkonoafua <nelsonosat@gmail.com>
2022-09-05 01:15:34 +0530

    commit to main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,20 @@ import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'home', component:HomeComponent },
+   
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'about', component:AboutComponent}, 
   {path: 'skills', component:SkillsComponent},
   {path: 'experience', component:ExperienceComponent},
-  {path: 'contact', component:ContactComponent}
+  {path: 'contact', component:ContactComponent},
+  {path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],    
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
